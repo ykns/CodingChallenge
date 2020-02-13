38bf0889aab7c20ee5c1f453413c95e03212d6ba
yax <yakeen.sewsanker@gmail.com>
2020-02-13 22:00:53 +0000

    App: Extracted base api url to environment
    Gives one central point to modify base api url and it also allows for
    easy switching of environments.

--- paymentsense-coding-challenge-website/src/app/services/paymentsense-coding-challenge-api.service.ts
+++ paymentsense-coding-challenge-website/src/app/services/paymentsense-coding-challenge-api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class PaymentsenseCodingChallengeApiService {
   constructor(private httpClient: HttpClient) {}
 
   public getHealth(): Observable<string> {
-    return this.httpClient.get('https://localhost:44341/health', { responseType: 'text' });
+    return this.httpClient.get(`${environment.baseApiUrl}/health`, { responseType: 'text' });
   }
 }
